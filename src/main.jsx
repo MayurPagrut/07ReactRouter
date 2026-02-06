155abe59478d2dfc0caf0aa81e55f46efdbf0c47
unknown <your.email@example.com>
2026-02-06 15:53:29 +0530

    2nd edit

--- src/main.jsx
+++ src/main.jsx
@@ -1,50 +1,42 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {
+  createHashRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom'
+
 import './index.css'
-import App from './App.jsx'
-import { createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
+
 import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
-import { Route } from 'react-router-dom'
 import User from './components/User/User.jsx'
 import Github from './components/Github/github.jsx'
 
-// const router =createBrowserRouter([
-//   {
-//     path:'/',
-//     element:<Layout/>,
-//     children:[
-//       {
-//         path:"",
-//         element:<Home/>
-//       },
-//       {
-//         path:"About",
-//         element:<About/>
-//       },
-//       {
-//         path:"Contact",
-//         element:<Contact/>
-//       }
-//     ]
-//   }
-// ])
-const router=createBrowserRouter(
+const router = createHashRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Layout/>}>
-      <Route path="About" element={<About/>}/>
-      <Route path="Contact" element={<Contact/>}/>
-      <Route path="" element={<Home/>}/>
-      <Route path="User/:userid" element={<User/>}/>
-      <Route path="Github" element={<Github/>}/>
+    <Route path="/" element={<Layout />}>
       
+      {/* Home */}
+      <Route index element={<Home />} />
+
+      {/* Pages */}
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="github" element={<Github />} />
+
+      {/* Dynamic */}
+      <Route path="user/:userid" element={<User />} />
+
     </Route>
   )
 )
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}/>
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 )
